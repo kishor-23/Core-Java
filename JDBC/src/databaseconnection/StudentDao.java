1cd568be6bd57715c379765d0385a4b91754f1f8
kish3574 <kish3574@CSL0689.global.chainsys.com>
2024-05-02 21:58:20 +0530

    added jdbc connection todolist

---
+++ JDBC/src/databaseconnection/StudentDao.java
@@ -0,0 +1,11 @@
+package databaseconnection;
+
+import java.sql.SQLException;
+
+public interface StudentDao {
+	void insertStudent(StudentImpl student) throws ClassNotFoundException, SQLException;
+    void readStudentDetails() throws ClassNotFoundException, SQLException;
+    void updateStudent(int id, String name, int age) throws ClassNotFoundException, SQLException ;
+    void deleteStudent(int id) throws ClassNotFoundException, SQLException;
+
+}
